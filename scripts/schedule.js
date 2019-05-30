c3302c412e542b05f3f9426e6b09ebde52e363f2
Sara Flemming <dotsara@gmail.com>
2019-05-30 15:19:04 -0700

    add first practice round
    I don't think the stylesheets will change much, so I spent a little bit
    of time tinkering with fonts and colors. (Might use CSS Grid in the
    future just for the fun of it. 🤷🏽‍♀️)

---
+++ scripts/schedule.js
@@ -0,0 +1,22 @@
+const reorderedMovies = document.getElementById('reordered-movies');
+const movies = [
+    "Jaws",
+    "Star Trek (2009)",
+    "Independence Day",
+    "Star Wars",
+    "Jurassic Park"
+];
+
+// found shuffle function from Googling
+const shuffleArray = array => array
+    .map(a => [Math.random(), a])
+    .sort((a, b) => a[0] - b[0])
+    .map(a => a[1]);
+
+const shuffledMovies = shuffleArray(movies);
+
+function mixUpMovies() {
+    for (const movie of shuffledMovies) {
+        reorderedMovies.innerHTML = reorderedMovies.innerHTML + `<li>${movie}</li>`;
+    }
+};
